65321b29117598d6c71a6ba63db6b1264eb2e5df
Jose Robles Colonia <kuro.rc10@gmail.com>
2022-06-05 16:44:06 -0700

    creando proyecto

---
+++ earthbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  earthbook
+//
+//  Created by Jose Robles on 5/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
