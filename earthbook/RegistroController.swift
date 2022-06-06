e3ff3ac58fc801d65d0a4525ea63b935e9827fbe
Jose Robles Colonia <kuro.rc10@gmail.com>
2022-06-05 20:32:52 -0700

    creando registro controller

---
+++ earthbook/RegistroController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistroController.swift
+//  earthbook
+//
+//  Created by Jose Robles on 5/06/22.
+//
+
+import UIKit
+
+class RegistroController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
